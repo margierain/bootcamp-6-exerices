fce3549f3f8ae990dadb6c2bcfc7b461eb8747b7
andela-MOchieng <margaret.ochieng@andela.com>
2016-04-14 15:15:03 +0300

    Update mod1.py

--- Day 3/Test/mod1.py
+++ Day 3/Test/mod1.py
@@ -2,7 +2,7 @@ def add(a,b):
     return a + b
 
 # print add(2,1) 
-''' the if __name__ == '__main': ensure thaat a file only works if it's done on the terminal, but the code wont work if mod1.py is importeed '''
+''' the if __name__ == '__main': ensure that a file only works if it's done on the terminal, but the code wont work if mod1.py is importeed '''
 
 if __name__ =='__main__':
     print add(1,2)            
